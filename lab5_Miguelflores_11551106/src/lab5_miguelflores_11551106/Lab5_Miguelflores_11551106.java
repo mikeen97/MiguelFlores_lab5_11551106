e62fa3cdb0a5c0023f8cc6b3f6dd6345fb3b5223
Miguel Flores <mike97@hotmail.es>
2017-05-26 13:37:15 -0600

    Agregue el Frame principal, empiezo hacer la vista del menu y poder agregar un cliente

---
+++ lab5_Miguelflores_11551106/src/lab5_miguelflores_11551106/Lab5_Miguelflores_11551106.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab5_miguelflores_11551106;
+
+/**
+ *
+ * @author Miguel Flores
+ */
+public class Lab5_Miguelflores_11551106 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
